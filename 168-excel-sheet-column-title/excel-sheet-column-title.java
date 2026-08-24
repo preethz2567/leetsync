e53556a8a89ec1ef56551678f55a636c31f5dba9
Preethi Durgaprasad <preethid.student@saveetha.ac.in>
2026-08-24 12:44:29 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (40.70%) - LeetSync

---
+++ 168-excel-sheet-column-title/excel-sheet-column-title.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String convertToTitle(int columnNumber) {
+        
+        StringBuilder ans = new StringBuilder();
+
+        while(columnNumber > 0){
+            columnNumber--;
+
+            int rem = columnNumber % 26;
+            char ch = (char)('A'+rem);
+            ans.append(ch);
+
+            columnNumber/=26;
+        }
+        return ans.reverse().toString();
+    }
+}
