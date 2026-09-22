517329a9abc22507615018a57eecda291f60a07c
Preethi Durgaprasad <preethid.student@saveetha.ac.in>
2026-09-22 11:53:15 +0530

    Time: 4 ms (81.28%) | Memory: 47.1 MB (40.01%) - LeetSync

---
+++ 735-asteroid-collision/asteroid-collision.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int asteroid : asteroids) {
+
+            boolean destroyed = false;
+
+            while (!stack.isEmpty() && stack.peek() > 0 && asteroid < 0) {
+
+                int top = stack.peek();
+
+                if (top < -asteroid) {
+                    stack.pop();
+                }
+                else if (top == -asteroid) {
+                    stack.pop();
+                    destroyed = true;
+                    break;
+                }
+                else {
+                    destroyed = true;
+                    break;
+                }
+            }
+
+            if (!destroyed) {
+                stack.push(asteroid);
+            }
+        }
+
+        int[] ans = new int[stack.size()];
+
+        for (int i = ans.length - 1; i >= 0; i--) {
+            ans[i] = stack.pop();
+        }
+
+        return ans;
+    }
+}
