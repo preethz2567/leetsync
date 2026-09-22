161d046014251cd8cba581a57c78ff3459925fa1
Preethi Durgaprasad <preethid.student@saveetha.ac.in>
2026-09-22 08:48:00 +0530

    Time: 1 ms (84.95%) | Memory: 42.8 MB (54.13%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.java
+++ 225-implement-stack-using-queues/implement-stack-using-queues.java
@@ -1,36 +1,30 @@
 class MyStack {
-
-    Queue<Integer>q1;
-    Queue<Integer>q2;
-
+    
+    Queue<Integer> q;
     public MyStack() {
-        q1 = new LinkedList<>();
-        q2 = new LinkedList<>();
+        q = new LinkedList<>();
     }
     
     public void push(int x) {
-        q2.offer(x);
+        
+        q.offer(x);
+        int size = q.size();
 
-        while(!q1.isEmpty()){
-            q2.offer(q1.poll());
+        for(int i=0 ; i<size-1 ; i++){
+            q.offer(q.poll());
         }
-
-        Queue<Integer> temp = q1;
-        q1=q2;
-        q2=temp;
     }
     
     public int pop() {
-        return q1.poll();
+        return q.poll();
     }
     
     public int top() {
-        return q1.peek();
-        
+        return q.peek();
     }
     
     public boolean empty() {
-        return q1.isEmpty();
+        return q.isEmpty();
     }
 }
 
